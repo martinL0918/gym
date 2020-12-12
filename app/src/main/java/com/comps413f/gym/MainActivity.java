6fd4b49fb39645a644199f5cbf22492d72cf4a5c
martinL0918 <martin641379@gmail.com>
2020-12-12 21:16:21 +0800

    added addaction.xml, orange_circle_background.xml and modified strings.xml

--- app/src/main/java/com/comps413f/gym/MainActivity.java
+++ app/src/main/java/com/comps413f/gym/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.addaction);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
         DatabaseReference newRef = database.getReference("Peter");
