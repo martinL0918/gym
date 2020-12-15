0bb5dbbbec01ab9d1659bbbf27043b5ddcf02d78
martinL0918 <martin641379@gmail.com>
2020-12-15 18:39:51 +0800

    added support of cardview and recyclerview

--- app/src/main/java/com/comps413f/gym/MainActivity.java
+++ app/src/main/java/com/comps413f/gym/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.actioncardview);
+        setContentView(R.layout.addaction);
         /*FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
         DatabaseReference newRef = database.getReference("Peter");
